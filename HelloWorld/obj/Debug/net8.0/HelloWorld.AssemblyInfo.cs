d060ae8ded80d8d6a598fbdec4854a73089cb91b
Matthew David <matthewdavid99@comcast.net>
2024-02-22 19:40:07 -0500

    objs

--- HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18c171c8cdfd4380e1e7b8b948eb1eeb99ec8c35")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72eadbb3d39be3ed5778d78af5fc561246abb956")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
